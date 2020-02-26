5aab0e5d4d98fd290fb835fd730c01b52ade6e0c
Adithya Paspu <apaspu6355@ucumberlands.edu>
2020-02-25 22:13:10 -0800

    New changes

---
+++ Myfirstplugin/ContactScripts.js
@@ -0,0 +1,14 @@
+﻿
+var Sdk = window.Sdk || {};
+
+(function () {
+
+    this.FirstnameOnchange = function (Executioncontext) {
+        var formcontext = Executioncontext.getFormContext();
+        var Fullname = formcontext.getAttributes("fullname").getValue();
+
+        alert("Name of the contact is" + Fullname );
+    }
+
+}
+).call(Sdk);
